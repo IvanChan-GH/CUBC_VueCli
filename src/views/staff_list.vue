de2ed0b334a18453e0ff7476e9d1cc2a7ec232dd
Vanniellel Ip <vanniellel@Vanniellels-MacBook-Pro.local>
2020-03-22 19:38:36 +0800

    add stafflist

--- src/views/staff_list.vue
+++ src/views/staff_list.vue
@@ -1,50 +1,57 @@
 <template>
     <v-container>
-        <v-row dense>
-          <v-col cols="12">
-          <v-card class="mb-3" color="#385F73" dark v-for="(staff, id) in boardgames" :key="id" @click="onclick(staff)">
-          <v-card-title class="headline">{{staff.name }}</v-card-title>
-        </v-card>
-      </v-col>
-        </v-row>
+      <v-row dense>
+        <v-col cols="12">
+          <v-card
+            class="mb-3"
+            color="#385F73"
+            dark
+            v-for="(staff, id) in staff"
+            :key="id"
+            @click="onclick(staff)"
+          >
+            <v-card-title class="headline">{{staff.username}}</v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
   </template>
   
   <script>
-  // import boardgame_detail from "../views/list/Boardgame_list.vue";
   export default {
     methods: {
       onclick: async function(staff) {
-       this.$router.push("/staff_list/" + staff.id);
+        this.$router.push("/stafflist/" + staff.id);
       }
     },
   
-    data() {
-      return {
+    data() { 
+        
+        // alert(this.staff);
+      return { 
         staff: []
+       
         //   books: [],
         //   presents: [],
         //   materials: []
       };
+     
     },
     async mounted() {
-      var response = await fetch("http://localhost:8080/user/staffList", {
+      var response = await fetch("http://localhost:8080/user/stafflist", {
         method: "GET",
         credentials: "same-origin"
-
-       
-      }); 
-      
-    
+      });
       // console.log(response);
       if (response.ok) {
         var data = await response.json();
-        this.staff = data.staff;
+        this.staff = data.User;
+        alert(JSON.stringify(data))
         //   this.books = data.books;
         //   this.presents = data.presents;
         //   this.materials = data.materials;
-        
-        alert( JSON.stringify(this.staff));
+  
+        // alert( JSON.stringify(this.boardgames));
         // alert(JSON.stringify(this.boardgames));
         // alert(JSON.stringify(this.books));
         // alert(JSON.stringify(this.presents));
